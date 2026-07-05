55bbf0eab61062f9ae1ba6c2e48381cbfee892a4
MGranda64 <matiasgranda64@gmail.com>
2026-07-05 03:10:36 -0500

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { RegistroClienteComponent } from './components/registro-cliente/registro-cliente.component';
 import { RegistroLocalizacionComponent } from './components/registro-localizacion/registro-localizacion.component';
+import { ConsultaPronosticoComponent } from './components/consulta-pronostico/consulta-pronostico.component';
+import { RegistroReporteComponent } from './components/registro-reporte/registro-reporte.component';
+import { RegistroObservacionComponent } from './components/registro-observacion/registro-observacion.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'registrar-cliente', component: RegistroClienteComponent },
   { path: 'registrar-localizacion', component: RegistroLocalizacionComponent },
+  { path: 'consulta-pronostico', component: ConsultaPronosticoComponent },
+  { path: 'registrar-observacion', component: RegistroObservacionComponent },
+  { path: 'registrar-reporte', component: RegistroReporteComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Cliente } from '../models/cliente.model';
 import { Localizacion } from '../models/localizacion.model';
+import { Pronostico } from '../models/pronostico.model';
+import { ObservacionReal } from '../models/observacion-real.model';
+import { ReportePrecision } from '../models/reporte-precision.model';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +19,66 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
+  
   registrarCliente(data: Cliente): Observable<any> {
     return this.http.post(`${this.baseUrl}/clientes/registro`, data);
   }
 
+  
   registrarLocalizacion(data: Localizacion): Observable<any> {
     return this.http.post(`${this.baseUrl}/localizaciones/registrarLocalizacion`, data);
   }
 
   consultarLocalizaciones(): Observable<Localizacion[]> {
     return this.http.get<Localizacion[]>(`${this.baseUrl}/localizaciones/listaTodos`);
+  }
+
+  
+  listaPorLocalizacion(idLocalizacion: number): Observable<Pronostico[]> {
+    return this.http.get<Pronostico[]>(`${this.baseUrl}/pronosticos/listaPorLocalizacion/${idLocalizacion}`);
+  }
+
+  registrarPronostico(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/pronosticos/registro`, data);
+  }
+
+  listarTodosPronosticos(): Observable<Pronostico[]> {
+    return this.http.get<Pronostico[]>(`${this.baseUrl}/pronosticos/listaTodos`);
+  }
+
+  
+  registrarObservacionReal(data: ObservacionReal): Observable<ObservacionReal> {
+    return this.http.post<ObservacionReal>(`${this.baseUrl}/observacionReal/registrarObservacionReal`, data);
+  }
+
+  listarObservacionesReales(): Observable<ObservacionReal[]> {
+    return this.http.get<ObservacionReal[]>(`${this.baseUrl}/observacionReal`);
+  }
+
+  buscarObservacionRealPorId(id: number): Observable<ObservacionReal> {
+    return this.http.get<ObservacionReal>(`${this.baseUrl}/observacionReal/${id}`);
+  }
+
+  actualizarObservacionReal(data: ObservacionReal): Observable<ObservacionReal> {
+    return this.http.put<ObservacionReal>(`${this.baseUrl}/observacionReal`, data);
+  }
+
+  eliminarObservacionReal(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/observacionReal/${id}`);
+  }
+
+
+  registrarReportePrecision(data: ReportePrecision): Observable<ReportePrecision> {   
+    return this.http.post<any>(`${this.baseUrl}/reportepresicion/insertaReporte`, data).pipe(
+      map(res => res.data)
+    );
+  }
+
+  listarReportesPorFecha(fecha: string): Observable<ReportePrecision[]> {
+    return this.http.get<ReportePrecision[]>(`${this.baseUrl}/reportepresicion/listaPorFecha?fecha=${fecha}`);
+  }
+
+  listarTodosReportes(): Observable<ReportePrecision[]> {
+    return this.http.get<ReportePrecision[]>(`${this.baseUrl}/reportepresicion/listaTodos`);
   }
 }
